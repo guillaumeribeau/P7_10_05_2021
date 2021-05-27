c0ca76006a93c68a018bf39d1017c4f5f889b19d
ribeau <guillaume>
2021-05-27 09:05:24 +0200

    no message

--- script/index.js
+++ script/index.js
@@ -10,7 +10,7 @@ const recherchePrincipale = document.getElementById("search");
 
 /**@param{tableau} */
 // fonction qui permet de creer les card des recettes
-export function AfficherRecettes(recette) {
+export function AfficherRecettes(recette,ingredientinfos) {
   const recetteCard = recette
     .map((recipe) => {
       return `<article class='col-3'>
@@ -27,12 +27,8 @@ export function AfficherRecettes(recette) {
 </div>
 
  <div class="contenu">
-   <div class='ingredient'>${recipe.ingredients
-     .map(
-       (x) =>
-         `<p><span class="bolder">${x.ingredient}:</span>${x.quantity}${x.unit}</p>`
-     )
-     .join("")}
+   <div class='ingredient'>${ingredientinfos}
+     
   </div>
  <div class='recettes'>${recipe.description}</div>
 </div>
@@ -40,58 +36,83 @@ export function AfficherRecettes(recette) {
 </div>
 </article>
 `;
-    })
-    .join("");
+    }).join("");
   const main = document.querySelector(".recettes__card");
   main.innerHTML = recetteCard;
+  ingredientDetails(recipes)
 }
 AfficherRecettes(recipes);
 
-// permet de filter le tableau avec methode map
-function filtrerTabeauCard(e) {
-  //let tableau = [];
-  let search = e.target.value.toLowerCase();
-  const filtrerCard = recipes.map((item) => {
-    const nomRecette = item.name.toLowerCase();
-    const description = item.description.toLowerCase();
-    const appareils = recipes.map((item) => {
-      const appareil = recipes.map((x) => x.appliance);
-     
-    });
-    const ingredients = recipes.map((item) => {
-      const ingredient = item.ingredients.map((x) => x["ingredient"]);
-      for (let val of ingredient) {
-        const valIngr = val.toLowerCase();
-     // console.log(valIngr)
-      if(valIngr.includes(search)){
-       // console.log(valIngr)
-           
-      }
-      }
-  
-    const ustensiles = recipes.map((item) => {
-      const ustensil = item.ustensils;
-      for (let valeur of ustensil) {
-      let valUst = valeur.toLowerCase();
-   
-    }
-    });
-    
-    
 
-    if (search.length > 2) {
-      for (let value of nomRecette) {
-        if (value.includes(search)) {
-          
-          //tableau.push(value);
-          //console.log(tableau);
-        }
-      }
-    }
-  });
-})
-AfficherRecettes(filtrerCard)
+
+
+
+
+function ingredientDetails(recipe) {
+let ingredientinfos=''
+const test=recipe.forEach((ingredient) => {
+ 
+if (ingredient.quantity) {
+  if (ingredient.unit && ingredient.quantity) {
+    ingredientinfos += `<li><strong>${ingredient.ingredient}</strong> : ${ingredient.quantity} ${ingredient.unit}</li>`;
+  } else {
+    ingredientinfos += `<li><strong>${ingredient.ingredient}</strong> : ${ingredient.quantity}</li>`;
+  }
+} else {
+  ingredientinfos += `<li><strong>${ingredient.ingredient}</strong></li>`;
 }
+});
+return AfficherRecettes(recipes, ingredientinfos);
+}
+
+
+
+
+
+// function search(searchValue) {
+//   // algo de recherche n°1 de la barre de recherche
+//   let recipesSorted = [];
+//   if (searchValue.length >= 3) {
+//     for (let recipe of recipes) {
+//       let nameArray = recipe.name.split(" ");
+//       let descArray = recipe.description.split(" ");
+//       let ingredientsArray = recipe.ingredients.map((ing) => {
+//         return ing.ingredient;
+//       });
+//       if (descArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
+//         recipesSorted.push(recipe);
+//       } else if (nameArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
+//         recipesSorted.push(recipe);
+//       } else if (ingredientsArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
+//         recipesSorted.push(recipe);
+//       }
+//     }
+//   }
+
+//   return recipesSorted;
+// }
+
+
+// Au keyup on execute la fonction filtrer card
+const inputP = document.getElementById("search");
+inputP.addEventListener("keyup", search);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // function filtrerTabeauCard(e){
 // let search= e.target.value.toLowerCase()
@@ -134,72 +155,70 @@ AfficherRecettes(filtrerCard)
 //   return filtrerCard;
 // };
 
-// Au keyup on execute la fonction filtrer card
-const inputP = document.getElementById("search");
-inputP.addEventListener("keyup", filtrerTabeauCard);
 
-// permet d'afficher seulement les elements de la liste encore présent dans le tableau filtrer
-// par la recherche principale.
+
+//permet d'afficher seulement les elements de la liste encore présent dans le tableau filtrer
+//par la recherche principale.
 const listAppareils = document.querySelectorAll(".list_appareils");
 const listIngredients = document.querySelectorAll(".list_ingredients");
 const listUstensiles = document.querySelectorAll(".list_ustensiles");
 
-// // retournes le tableau trier recherche principale
-// inputP.addEventListener("keyup", function (e) {
-//   let search = e.target.value.toLowerCase();
-//   const cardRestante = filtrerTableauCardTrier(search);
-//   const appareilRestant = cardRestante.map((item) =>
-//     item.appliance.toLowerCase()
-//   );
-//   const ingredientRestant = cardRestante.map((item) => {
-//     const ingredient = item.ingredients.map((x) => x["ingredient"]);
-//     for (let val of ingredient) {
-//       const valIngr = val.toLowerCase();
-//       return valIngr;
-//     }
-//   });
-//   const ustensilRestant = cardRestante.map((item) => {
-//     const ustensil = item.ustensils;
-//     for (let valeur of ustensil) {
-//       let valUst = valeur.toLowerCase();
-//       return valUst;
-//     }
-//   });
+// retournes le tableau trier recherche principale
+inputP.addEventListener("keyup", function (e) {
+  let search = e.target.value.toLowerCase();
+  const cardRestante = filtrerTableauCardTrier(search);
+  const appareilRestant = cardRestante.map((item) =>
+    item.appliance.toLowerCase()
+  );
+  const ingredientRestant = cardRestante.map((item) => {
+    const ingredient = item.ingredients.map((x) => x["ingredient"]);
+    for (let val of ingredient) {
+      const valIngr = val.toLowerCase();
+      return valIngr;
+    }
+  });
+  const ustensilRestant = cardRestante.map((item) => {
+    const ustensil = item.ustensils;
+    for (let valeur of ustensil) {
+      let valUst = valeur.toLowerCase();
+      return valUst;
+    }
+  });
 
-//   if (search.length > 2) {
-//     // trie dans les appareils
-//     for (let valeur of listAppareils) {
-//       let textValue =
-//         valeur.textContent.toLowerCase() || valeur.innerText.toLowerCase();
+  if (search.length > 2) {
+    // trie dans les appareils
+    for (let valeur of listAppareils) {
+      let textValue =
+        valeur.textContent.toLowerCase() || valeur.innerText.toLowerCase();
 
-//       if (appareilRestant.includes(textValue)) {
-//         valeur.style.display = "";
-//       } else {
-//         valeur.style.display = "none";
-//       }
-//     }
-//     // trie dans les ingredients
-//     for (let valeur of listIngredients) {
-//       let textValue =
-//         valeur.textContent.toLowerCase() || valeur.innerText.toLowerCase();
-//       if (ingredientRestant.includes(textValue)) {
-//         valeur.style.display = "";
-//       } else {
-//         valeur.style.display = "none";
-//       }
-//     }
-//     for (let valeur of listUstensiles) {
-//       let textValue =
-//         valeur.textContent.toLowerCase() || valeur.innerText.toLowerCase();
-//       if (ustensilRestant.includes(textValue)) {
-//         valeur.style.display = "";
-//       } else {
-//         valeur.style.display = "none";
-//       }
-//     }
-//   } else {
-//     afficherListTrier(listIngredients, search);
-//     afficherListTrier(listUstensiles, search);
-//     afficherListTrier(listAppareils, search);
-//   }
-// });
+      if (appareilRestant.includes(textValue)) {
+        valeur.style.display = "";
+      } else {
+        valeur.style.display = "none";
+      }
+    }
+    // trie dans les ingredients
+    for (let valeur of listIngredients) {
+      let textValue =
+        valeur.textContent.toLowerCase() || valeur.innerText.toLowerCase();
+      if (ingredientRestant.includes(textValue)) {
+        valeur.style.display = "";
+      } else {
+        valeur.style.display = "none";
+      }
+    }
+    for (let valeur of listUstensiles) {
+      let textValue =
+        valeur.textContent.toLowerCase() || valeur.innerText.toLowerCase();
+      if (ustensilRestant.includes(textValue)) {
+        valeur.style.display = "";
+      } else {
+        valeur.style.display = "none";
+      }
+    }
+  } else {
+    afficherListTrier(listIngredients, search);
+    afficherListTrier(listUstensiles, search);
+    afficherListTrier(listAppareils, search);
+  }
+});
