af65b2aa31190d454bbe08034fec0aec42cff741
ribeau <guillaume>
2021-06-01 12:15:07 +0200

    no message

--- script/index.js
+++ script/index.js
@@ -5,28 +5,29 @@ import {
   afficherLiUstensil,
   afficherIngredient,
 } from "./dropdown.js";
-
-const recherchePrincipale = document.getElementById("search");
 const main = document.querySelector(".recettes__card");
-//--------------BUILD LES CARDS------------//////////////////
+const recherchePrincipale = document.getElementById("search");
+
+//---------------------BUILD LES CARDS----------------////////////////
+
 /**@param{tableau} */
 // fonction qui permet de creer les card des recettes
 export function AfficherRecettes(recette) {
-   const recetteCard = recette
+  const recetteCard = recette
     .map((recipe) => {
-let ingredientinfos=''
-const ingredient = recipe.ingredients.map((ingredient) =>{
-if (ingredient.quantity) {
-  if (ingredient.unit && ingredient.quantity) {
-    ingredientinfos += `<li><strong>${ingredient.ingredient}</strong>:${ingredient.quantity}${ingredient.unit}</li>`;
-  } else {
-    ingredientinfos += `<li><strong>${ingredient.ingredient}</strong>:${ingredient.quantity}</li>`;
-  }
-} else {
-  ingredientinfos += `<li><strong>${ingredient.ingredient}</strong></li>`;
-}
-});
- return `<article class='col-3'>
+      let ingredientinfos = "";
+      const ingredient = recipe.ingredients.map((ingredient) => {
+        if (ingredient.quantity) {
+          if (ingredient.unit && ingredient.quantity) {
+            ingredientinfos += `<li>${ingredient.ingredient}:${ingredient.quantity}${ingredient.unit}</li>`;
+          } else {
+            ingredientinfos += `<li>${ingredient.ingredient}:${ingredient.quantity}</li>`;
+          }
+        } else {
+          ingredientinfos += `<li>${ingredient.ingredient}</li>`;
+        }
+      });
+      return `<article class='col-3'>
 <div class='img'></div>
 
 <div class='recette__complete'>     
@@ -38,7 +39,7 @@ if (ingredient.quantity) {
  <span class='time'>${recipe.time}min</span>
 </div>
 </div>
-
+  
  <div class="contenu">
    <div class='ingredient'>${ingredientinfos}</div>
  <div class='recettes'>${recipe.description}</div>
@@ -47,52 +48,39 @@ if (ingredient.quantity) {
 </div>
 </article>
 `;
-    }).join("");
-
-  main.innerHTML = recetteCard;
+    })
+    .join("");
  
+  main.innerHTML = recetteCard;
 }
 AfficherRecettes(recipes);
 
-//---------------------------------------------//
-//-----------ALGO n°2 -----------------------//
-function search(e) {
-  
-  let searchValue= e.target.value.toLowerCase();
-  let recipesSorted = [];
-  if (searchValue.length >= 3) {
-    for (let recipe of recipes) {
-      let nameArray = recipe.name.split(" ");
-      let descArray = recipe.description.split(" ");
-      let ingredientsArray = recipe.ingredients.map((ing) => {
-        return ing.ingredient;
-      });
-      if (descArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
-        recipesSorted.push(recipe);
-      } else if (nameArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
-        recipesSorted.push(recipe);
-      } else if (ingredientsArray.some((el) => el.toLowerCase().match(searchValue.toLowerCase()))) {
-        recipesSorted.push(recipe);
-      }
-     
-    }
- return recipesSorted;
-  }
+//---------------------------------------------------------------------//
+//---------------------------FILTRE ALGOS n1 avec filter()------------///
 
+function filtrerTabeauCard(e) {
+  let search = e.target.value.toLowerCase();
+  if (search.length > 2) {
+    const filtrerCard = filtrerTableauCardTrier(search,recipes)
+    AfficherRecettes(filtrerCard);
+    // si aucune occurence alors on affiche toutes les recettes
+    if (filtrerCard.length === 0) {
+      recherchePrincipale.value = " ";
+      main.innerHTML+= `<span class='message_erreur'>Aucune recette ne correspond à votre critères... vous pouvez chercher "tarte aux pommes,"poisson",...</span>`
+      
+    }
+  } else {
+    AfficherRecettes(recipes);
+  }
 }
 
-// Au keyup on execute la fonction filtrer card
-const inputP = document.getElementById("search");
-inputP.addEventListener("keyup", search);
-
-
-//fonction qui retourne le tableau filtrer recherche principale.
-const filtrerTableauCardTrier = function (search) {
-  const filtrerCard = recipes.filter((item) => {
-    const ingredient = item.ingredients.map((x) => x["ingredient"]);
+// fonction qui retourne le tableau filtrer recherche principale.
+export const filtrerTableauCardTrier = function (search, array) {
+  const filtrerCard = array.filter((item) => {
+    const ingredient = item.ingredients.map((x) => x.ingredient);
+   
     return (
       item.name.toLowerCase().includes(search) ||
-      item.appliance.toString().toLowerCase().includes(search) ||
       item.description.includes(search) ||
       ingredient.toString().toLowerCase().includes(search)
     );
@@ -101,11 +89,14 @@ const filtrerTableauCardTrier = function (search) {
   return filtrerCard;
 };
 
+// Au keyup on execute la fonction filtrer card
+const inputP = document.getElementById("search");
+inputP.addEventListener("keyup", filtrerTabeauCard);
 
-//--------------------------------------------------------//
-//-----------------FILTRES LES LISTES--------------------//
-//permet d'afficher seulement les elements de la liste encore présent dans le tableau filtrer
-//par la recherche principale.
+//-------------------TRIER LES ELEMENTS DE LA LISTE--------------------//
+//---------------------------------------------------------------------//
+// permet d'afficher seulement les elements de la liste encore présent dans le tableau filtrer
+// par la recherche principale.
 const listAppareils = document.querySelectorAll(".list_appareils");
 const listIngredients = document.querySelectorAll(".list_ingredients");
 const listUstensiles = document.querySelectorAll(".list_ustensiles");
@@ -113,13 +104,13 @@ const listUstensiles = document.querySelectorAll(".list_ustensiles");
 // retournes le tableau trier recherche principale
 inputP.addEventListener("keyup", function (e) {
   let search = e.target.value.toLowerCase();
-  const cardRestante = filtrerTableauCardTrier(search);
+  const cardRestante = filtrerTableauCardTrier(search, recipes);
   const appareilRestant = cardRestante.map((item) =>
     item.appliance.toLowerCase()
   );
   const ingredientRestant = cardRestante.map((item) => {
-    const ingredient = item.ingredients.map((x) => x["ingredient"]);
-    for (let val of ingredient) {
+    const ingredient = item.ingredients.map((x) => x.ingredient);
+     for (let val of ingredient) {
       const valIngr = val.toLowerCase();
       return valIngr;
     }
@@ -135,7 +126,7 @@ inputP.addEventListener("keyup", function (e) {
   if (search.length > 2) {
     // trie dans les appareils
     for (let valeur of listAppareils) {
-      let textValue =
+     let textValue =
         valeur.textContent.toLowerCase() || valeur.innerText.toLowerCase();
 
       if (appareilRestant.includes(textValue)) {
